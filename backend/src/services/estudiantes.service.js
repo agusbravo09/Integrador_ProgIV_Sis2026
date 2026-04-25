fe73c2d22dddce0fcc707a07fa7fc212360dda1c
Marquitos Leiva <marcleiva623@gmail.com>
2026-04-25 16:19:43 -0300

    estructura básica de backend con endpoint estudiantes

---
+++ backend/src/services/estudiantes.service.js
@@ -0,0 +1,12 @@
+
+// TODO: Implementar logica a base de datos
+
+export const getAll = async () => 'getAll ok';
+
+export const getById = async (id) => `getById ok - id: ${id}`;
+
+export const create = async (datos) => 'create ok';
+
+export const update = async (id, datos) => `update ok - id: ${id}`;
+
+export const remove = async (id) => `remove ok - id: ${id}`;
