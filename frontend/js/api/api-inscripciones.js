3dd44cc8369d4c023714bc758366a6e50395d7ea
maticoppes <matiasareguaticoppes@gmail.com>
2026-06-16 15:27:04 -0300

    ..

--- frontend/js/api/api-inscripciones.js
+++ frontend/js/api/api-inscripciones.js
@@ -1,9 +1,19 @@
 // api/api-inscripciones.js
 import { API_BASE_URL } from './api-cursos.js';
 
+// Obtiene el token y crea las cabeceras requeridas
+function getHeaders(extraHeaders = {}) {
+    const token = localStorage.getItem('token');
+    return {
+        'Content-Type': 'application/json',
+        ...(token ? { 'Authorization': `Bearer ${token}` } : {}),
+        ...extraHeaders
+    };
+}
+
 async function getInscripcionesByCurso(idCurso) {
     try {
-        const response = await fetch(`${API_BASE_URL}/cursos/${idCurso}/inscripciones`);
+        const response = await fetch(`${API_BASE_URL}/cursos/${idCurso}/inscripciones`,{ headers: getHeaders()});
         const data = await response.json();
         console.log(`GET /cursos/${idCurso}/inscripciones response:`, data);
         return data;
